7939929b16b3c8ecb9ac008379aa72240418e4e2
QuestSWE <thacrank0001@gmail.com>
2025-01-15 03:52:59 -0500

    Main method logic completed. CountWordsInFile method in the making.

--- wc_tool/qwc/qwc/Program.cs
+++ wc_tool/qwc/qwc/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.IO;
 using System.Linq;
 
@@ -8,7 +9,33 @@ namespace qwc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Error: Invalid option. Use '-c' followed by a file name (e.g., program.exe -c file.txt");
+                return;
+            }
+            else if (args[0] != "-c")
+            {
+                Console.WriteLine("Error: Invalid option. Use '-c' to specify the word count operation.");
+                return;
+            }
+            else if (!File.Exists(args[1]))
+            {
+                Console.WriteLine($"Error: The specified file {args[1]} does not exist.");
+                return;
+            }
+            else
+            {
+                CountWordInFile(args[1]);
+            }
+
+            static void CountWordInFile(string filePath)
+            {
+
+                string text = File.ReadAllText(filePath);
+                string[] words = text.Split();
+            }
+
         }
     }
 }
